c693c6178563fce7f863b08299a85e6f072986db
Andrew Thorp <andrewpthorp@gmail.com>
2013-04-22 10:23:06 -0500

    basic layout work

--- danielthorp.rb
+++ danielthorp.rb
@@ -7,7 +7,23 @@ class DanielThorp < Sinatra::Base
   set :session_scret, ENV["SESSION_SECRET"] || "abc123"
 
   get "/" do
+    @action = 'home'
     haml :index, layout: true
+  end
+
+  get "/reel" do
+    @action = 'reel'
+    haml :reel, layout: true
+  end
+
+  get "/photos" do
+    @action = 'photos'
+    haml :photos, layout: true
+  end
+
+  get "/contact" do
+    @action = 'contact'
+    haml :contact, layout: true
   end
 
 end
